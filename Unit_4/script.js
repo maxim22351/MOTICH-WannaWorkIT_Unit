c64a6f024b8411edc4746cb94cb234117e320f33
maxim <max.furman2014@gmail.com>
2022-02-17 16:29:09 +0200

    Unit_4

---
+++ Unit_4/script.js
@@ -0,0 +1,42 @@
+const blockButton = document.querySelector('.block_btn')
+
+
+blockButton.addEventListener('click', function (e) {
+
+    switch (e.target.style.backgroundColor){
+        case 'red':
+            document.body.style.backgroundColor = 'red';
+            break;
+
+        case 'orange':
+            for (let i = 0; i < this.children.length; i++) {
+                if (this.children[i].style.backgroundColor === 'orange') continue;
+                this.children[i].style.color = 'white';
+            }
+            break;
+
+        case 'yellow':
+            this.lastElementChild.textContent = 'другой';
+            break;
+
+        case 'green':
+            document.body.children[1].style.display = 'block'
+            break;
+
+        case 'blue':
+            this.firstElementChild.style.display = 'none';
+            break;
+
+        case 'purple':
+            if (this.firstElementChild.style.display === '') alert('Кнопка есть');
+            this.firstElementChild.style.display = '';
+            break;
+    }
+})
+
+blockButton.children[4].addEventListener('copy',function (){
+    this.insertAdjacentHTML(
+            'beforebegin',
+            '<button style="background: deepskyblue">Button 5</button>'
+        )
+})
